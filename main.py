dbb15f05766007a7fb621fe9f265a14f5629278e
kr4b <40267916+kr4b@users.noreply.github.com>
2023-12-22 09:11:39 +0100

    Add multiple diffusion timesteps

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ conic = torch.inverse(torch.diag(torch.ones(d, device="cuda")) * 0.1)
 sample_mean = torch.tensor([0.0, 0.0], device="cuda").reshape(1, 1, d, 1)
 samples = means.unsqueeze(-1) - sample_mean
 powers = -0.5 * (samples.transpose(-1, -2) @ (conic @ samples))
-values = torch.exp(powers)
+values = torch.exp(powers).squeeze()
 values = values / torch.max(values)
 
 scaling = torch.exp(scaling)
@@ -50,31 +50,12 @@ model = Network().cuda()
 optim = torch.optim.Adam(model.parameters())
 model.train()
 
-N = 1000
+N = 10000
 log_step = N / 100
 training_error = []
 
 for epoch in range(N):
-    new_means = means.clone()
-    new_values = values.clone()
-    deltas = torch.zeros((nx, ny, 3), device="cuda")
-    for i in range(ny):
-        for j in range(nx):
-            xyu = torch.tensor([tx[j], ty[i], values[j,i]], device="cuda")
-            delta = model(xyu)
-            deltas[j,i] = delta / 0.01
-            new_means[j,i] += delta[:2]
-            new_values[j,i] += delta[2]
-
     samples = torch.rand((100, 2), device="cuda") * 2.0 - 1.0
-    derivatives = gaussians.gaussian_derivative2(
-        means, covariances, opacities, values, samples, d) # 100, nx, ny, d
-    dx = derivatives.sum(dim=(1,2)) # 100, d
-
-    sample_values = gaussians.sample_gaussians(
-        means, covariances, opacities, values, samples, d) # 100, nx, ny, d
-    dt = sample_values * deltas[:,:,2].reshape(1, nx, ny, 1)
-    dt = dt.sum(dim=(1,2,3)) # 100
 
     boundaries = torch.cat((-torch.ones(25, device="cuda"), torch.ones(25, device="cuda")))
     bc_samples = torch.zeros((100, 2), device="cuda")
@@ -82,17 +63,45 @@ for epoch in range(N):
     bc_samples[50:,1] = boundaries
     bc_samples[:50,1] = torch.rand(50, device="cuda") * 2.0 - 1.0
     bc_samples[:50,0] = boundaries
-    bc_sample_values = gaussians.sample_gaussians(
-        new_means, covariances, opacities, new_values, samples, d) # 100, nx, ny, d
 
-    diffusion_loss = torch.mean((dt - dx.sum(dim=-1)) ** 2)
-    bc_loss = torch.mean(bc_sample_values ** 2)
-    conservation_loss = torch.mean(deltas[:2].sum(dim=-1) ** 2)
+    new_means = means.clone()
+    new_values = values.clone()
+
+    diffusion_loss = torch.zeros(1, device="cuda")
+    bc_loss = torch.zeros(1, device="cuda")
+    conservation_loss = torch.zeros(1, device="cuda")
+
+    for _ in range(5):
+        derivatives = gaussians.gaussian_derivative2(
+            new_means, covariances, opacities, new_values, samples, d) # 100, nx, ny, d
+        dx = derivatives.sum(dim=(1,2)) # 100, d
+
+        sample_values = gaussians.sample_gaussians(
+            new_means, covariances, opacities, new_values, samples, d) # 100, nx, ny, d
+
+        xyu = torch.cat((new_means, new_values.unsqueeze(-1)), dim=-1)
+        delta = model(xyu)
+        deltas = delta / 0.01
+        new_means = new_means + delta[:,:,:2]
+        new_values = new_values + delta[:,:,2]
+
+        dt = sample_values * deltas[:,:,2].reshape(1, nx, ny, 1)
+        dt = dt.sum(dim=(1,2,3)) # 100
+        bc_sample_values = gaussians.sample_gaussians(
+            new_means, covariances, opacities, new_values, samples, d) # 100, nx, ny, d
+
+        diffusion_loss += torch.mean((dt - dx.sum(dim=-1)) ** 2)
+        bc_loss += torch.mean(bc_sample_values ** 2)
+        conservation_loss += torch.mean(deltas[:2].sum(dim=-1) ** 2)
+
     loss = diffusion_loss + bc_loss + conservation_loss
 
     loss.backward()
     optim.step()
     optim.zero_grad()
+
+    if loss.item() < 0.2:
+        break
 
     if epoch % log_step == 0:
         training_error.append(loss.item())
@@ -116,33 +125,30 @@ model.eval()
 with torch.no_grad():
     new_means = means.clone()
     new_values = values.clone()
-    for i in range(ny):
-        for j in range(nx):
-            xyu = torch.tensor([tx[j], ty[i], values[j,i]], device="cuda")
-            delta = model(xyu)
-            new_means[j,i] += delta[:2]
-            new_values[j,i] += delta[2]
+    for i in range(10):
+        xyu = torch.cat((new_means, new_values.unsqueeze(-1)), dim=-1)
+        delta = model(xyu)
+        new_means = new_means + delta[:,:,:2]
+        new_values = new_values + delta[:,:,2]
 
-img2 = gaussians.sample_gaussians_img(
-    new_means, covariances, opacities, new_values, res, res, d
-).detach().cpu().numpy()
+        img2 = gaussians.sample_gaussians_img(
+            new_means, covariances, opacities, new_values, res, res, d
+        ).detach().cpu().numpy()
 
-img3 = gaussians.sample_gaussians_img(
-    means, covariances, opacities, new_values, res, res, d
-).detach().cpu().numpy()
+        img3 = gaussians.sample_gaussians_img(
+            means, covariances, opacities, new_values, res, res, d
+        ).detach().cpu().numpy()
 
-img4 = gaussians.sample_gaussians_img(
-    new_means, covariances, opacities, values, res, res, d
-).detach().cpu().numpy()
+        img4 = gaussians.sample_gaussians_img(
+            new_means, covariances, opacities, values, res, res, d
+        ).detach().cpu().numpy()
 
-fig = plt.figure()
-ax = fig.subplots(2, 2)
-im = ax[0,0].imshow(img1)
-plt.colorbar(im)
-im = ax[0,1].imshow(img2)
-plt.colorbar(im)
-im = ax[1,0].imshow(img3)
-plt.colorbar(im)
-im = ax[1,1].imshow(img4)
-plt.colorbar(im)
-plt.savefig("results.png")
+        fig = plt.figure()
+        ax = fig.subplots(2, 2)
+        im = ax[0,0].imshow(img1, vmin=np.min(img1), vmax=np.max(img1))
+        ax[0,1].imshow(img2, vmin=np.min(img1), vmax=np.max(img1))
+        ax[1,0].imshow(img3, vmin=np.min(img1), vmax=np.max(img1))
+        ax[1,1].imshow(img4, vmin=np.min(img1), vmax=np.max(img1))
+        cbar_ax = fig.add_axes([0.9, 0.1, 0.025, 0.8])
+        fig.colorbar(im, cax=cbar_ax)
+        plt.savefig("results{}.png".format(i))
