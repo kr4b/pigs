Fix second order derivative

gaussians.py
@@ -12,8 +12,8 @@ from matplotlib.patches import Ellipse
 
 from diff_gaussian_rasterization import GaussianRasterizationSettings, GaussianRasterizer
 
-def plot_gaussians(means, covariances, values, opacities, d):
-    nx, ny = covariances.shape[:2]
+def plot_gaussians(means, covariances, opacities, values):
+    nx, ny, d = means.shape
 
     means = means.cpu().numpy()
     values = values.cpu().numpy()
@@ -29,8 +29,8 @@ def plot_gaussians(means, covariances, values, opacities, d):
     for i in range(ny):
         for j in range(nx):
             color = matplotlib.cm.get_cmap('viridis')
-            ellipse = Ellipse(xy=(0.0, 0.0), width=25.0, height=25.0, fc=color(values[i,j]), alpha=opacities[i,j].item())
-            affine = Affine2D(covariance[i*nx+j,:,:]).translate(*means[i,j,:2])
+            ellipse = Ellipse(xy=(0.0, 0.0), width=25.0, height=25.0, fc=color(values[j,i,0]), alpha=opacities[i,j].item())
+            affine = Affine2D(covariance[j*ny+i,:,:]).translate(*means[j,i,:2])
             ellipse.set_transform(affine + ax.transData)
 
             ax.add_patch(ellipse)
@@ -65,11 +65,10 @@ def plot_gaussians(means, covariances, values, opacities, d):
 #        for l in range(h):
 #            img[l,k] = compute_gaussian(densities, opacities, values, nx, ny, k, l)[0]
 
-def sample_gaussians(means, covariances, opacities, values, samples, d):
-    nx, ny = covariances.shape[:2]
+def sample_gaussians(means, conics, opacities, values, samples):
+    nx, ny, d = means.shape
 
     x = samples.reshape(-1, 1, 1, d, 1) - means.reshape(1, nx, ny, d, 1)
-    conics = torch.inverse(covariances)
     powers = -0.5 * (x.transpose(-1, -2) @ (conics @ x))
     densities = torch.exp(powers).reshape(-1, nx, ny, 1)
 
@@ -81,22 +80,25 @@ def sample_gaussians(means, covariances, opacities, values, samples, d):
 
     return res
 
-def _sample_gaussians_region(d, center, size, dx):
+def _sample_gaussians_region(d, centers, size, dx):
     t = []
     for i in range(d):
         half_size = (size[i] - 1) / 2
-        t.append(center[i] + torch.linspace(-half_size * dx[i], half_size * dx[i], size[i]))
+        t.append(torch.linspace(-half_size, half_size, size[i]).cuda() * dx[i])
 
     grid = torch.meshgrid(t, indexing="xy")
-    samples = torch.stack(grid, dim=-1).reshape(-1, d)
+    samples = centers.reshape(-1, 1, d) + torch.stack(grid, dim=-1).reshape(1, -1, d)
 
     return samples
 
-def sample_gaussians_region(means, covariances, opacities, values, d, center, size, dx):
+def sample_gaussians_region(means, conics, opacities, values, center, size, dx):
+    nx, ny, d = means.shape
     samples = _sample_gaussians_region(d, center, size, dx)
-    return sample_gaussians(means, covariances, opacities, values, samples, d)
+    return sample_gaussians(means, conics, opacities, values, samples)
 
-def sample_gaussians_img(means, covariances, opacities, values, w, h, d):
+def sample_gaussians_img(means, conics, opacities, values, w, h):
+    nx, ny, d = means.shape
+
     tx = torch.linspace(-1, 1, w).cuda()
     ty = torch.linspace(-1, 1, h).cuda()
     gx, gy = torch.meshgrid((tx, ty), indexing="xy")
@@ -106,43 +108,43 @@ def sample_gaussians_img(means, covariances, opacities, values, w, h, d):
     if d == 2:
         samples = torch.stack((gx, gy), dim=-1).reshape(w * h, d)
 
-    img = sample_gaussians(means, covariances, opacities, values, samples, d)
+    img = sample_gaussians(means, conics, opacities, values, samples)
     img = img.sum(dim=(1,2))
 
     return img.reshape(w, h, -1)
 
-def gaussian_derivative(means, covariances, opacities, values, samples, d):
-    nx, ny = covariances.shape[:2]
+def gaussian_derivative(means, conics, opacities, values, samples):
+    nx, ny, d = means.shape
 
     x = samples.reshape(-1, 1, 1, d, 1) - means.reshape(1, nx, ny, d, 1)
-    conics = torch.inverse(covariances)
-    powers = -0.5 * (x.transpose(-1, -2) @ (conics @ x))
-    densities = torch.exp(powers).reshape(-1, nx, ny, 1)
-    derivatives = -(conics @ x).reshape(-1, nx, ny, d) * densities
+    inv_prod = conics @ x
+    powers = -0.5 * (x.transpose(-1, -2) @ inv_prod)
+    densities = torch.exp(powers).reshape(-1, nx, ny, 1, 1)
+    derivatives = -inv_prod.reshape(-1, nx, ny, d, 1) * densities
 
-    opacities = opacities.reshape(1, nx, ny, 1)
-    values = values.reshape(1, nx, ny, -1)
+    opacities = opacities.reshape(1, nx, ny, 1, 1)
+    values = values.reshape(1, nx, ny, 1, -1)
 
     res = derivatives * opacities * values
-    res = res / densities.sum(dim=(1,2)).reshape(-1, 1, 1, 1)
+    res = res / densities.sum(dim=(1,2)).reshape(-1, 1, 1, 1, 1)
 
     return res
 
-def gaussian_derivative2(means, covariances, opacities, values, samples, d):
-    nx, ny = covariances.shape[:2]
+def gaussian_derivative2(means, conics, opacities, values, samples):
+    nx, ny, d = means.shape
 
     x = samples.reshape(-1, 1, 1, d, 1) - means.reshape(1, nx, ny, d, 1)
-    conics = torch.inverse(covariances)
-    powers = -0.5 * (x.transpose(-1, -2) @ (conics @ x))
-    densities = torch.exp(powers).reshape(-1, nx, ny, 1)
+    inv_prod = conics @ x
+    powers = -0.5 * (x.transpose(-1, -2) @ inv_prod)
+    densities = torch.exp(powers).reshape(-1, nx, ny, 1, 1)
     ones = torch.ones(x.shape, device="cuda")
-    derivatives = ((conics @ x) ** 2  - (conics @ ones)).reshape(-1, nx, ny, d) * densities
+    derivatives = (inv_prod @ inv_prod.transpose(-1, -2) - conics).reshape(-1, nx, ny, d, d) * densities
 
-    opacities = opacities.reshape(1, nx, ny, 1)
-    values = values.reshape(1, nx, ny, -1)
+    opacities = opacities.reshape(1, nx, ny, 1, 1)
+    values = values.reshape(1, nx, ny, 1, -1)
 
     res = derivatives * opacities * values
-    res = res / densities.sum(dim=(1,2)).reshape(-1, 1, 1, 1)
+    res = res / densities.sum(dim=(1,2)).reshape(-1, 1, 1, 1, 1)
 
     return res
 
@@ -209,24 +211,25 @@ class UnitTests(unittest.TestCase):
         for element in expected:
             anyTrue = False
             for sample in samples:
-                anyTrue |= torch.all(torch.isclose(sample, element))
+                anyTrue |= torch.all(torch.isclose(sample, element.cuda()))
 
             self.assertTrue(anyTrue)
 
     def test_sample_region_2d_2(self):
-        samples = _sample_gaussians_region(2, torch.zeros(2), (2, 2), torch.ones(2))
-        self.assertEqual(samples.shape, (4, 2))
+        samples = _sample_gaussians_region(2, torch.zeros((2, 2), device="cuda"), (2, 2), torch.ones(2, device="cuda"))
+        self.assertEqual(samples.shape, (2, 4, 2))
         expected = [
             torch.tensor([-0.5, -0.5]),
             torch.tensor([0.5, -0.5]),
             torch.tensor([-0.5, 0.5]),
             torch.tensor([0.5, 0.5]),
         ]
-        self.contains(expected, samples)
+        self.contains(expected, samples[0])
+        self.contains(expected, samples[1])
 
     def test_sample_region_2d_3(self):
-        samples = _sample_gaussians_region(2, torch.tensor([0.0, 0.5]), (3, 3), torch.ones(2) * 0.5)
-        self.assertEqual(samples.shape, (9, 2))
+        samples = _sample_gaussians_region(2, torch.tensor([[0.0, 0.5]], device="cuda"), (3, 3), torch.ones(2, device="cuda") * 0.5)
+        self.assertEqual(samples.shape, (1, 9, 2))
         expected = [
             torch.tensor([-0.5, 0.0]),
             torch.tensor([0.0, 0.0]),
@@ -238,11 +241,11 @@ class UnitTests(unittest.TestCase):
             torch.tensor([0.0, 1.0]),
             torch.tensor([0.5, 1.0]),
         ]
-        self.contains(expected, samples)
+        self.contains(expected, samples[0])
 
     def test_sample_region_3d_2(self):
-        samples = _sample_gaussians_region(3, torch.tensor([2.0, -1.0, 0.0]), (2, 2, 2), torch.ones(3) * 2.0)
-        self.assertEqual(samples.shape, (8, 3))
+        samples = _sample_gaussians_region(3, torch.tensor([[2.0, -1.0, 0.0]], device="cuda"), (2, 2, 2), torch.ones(3, device="cuda") * 2.0)
+        self.assertEqual(samples.shape, (1, 8, 3))
         expected = [
             torch.tensor([1.0, -2.0, -1.0]),
             torch.tensor([3.0, -2.0, -1.0]),
@@ -253,7 +256,7 @@ class UnitTests(unittest.TestCase):
             torch.tensor([1.0, 0.0, 1.0]),
             torch.tensor([3.0, 0.0, 1.0]),
         ]
-        self.contains(expected, samples)
+        self.contains(expected, samples[0])
 
 if __name__ == '__main__':
     unittest.main()

test_derivatives.py
@@ -17,48 +17,130 @@ gx, gy = torch.meshgrid((tx,ty), indexing="xy")
 means = torch.stack((gx,gy), dim=-1)
 scaling = torch.ones((nx,ny,d), device="cuda") * -4.0
 transform = torch.zeros((nx,ny, d * (d - 1) // 2), device="cuda")
+for i in range(ny):
+    for j in range(nx):
+        transform[j,i,0] = 0.5
 opacities = torch.ones((nx,ny), device="cuda") * 0.5
 conic = torch.inverse(torch.diag(torch.ones(d, device="cuda")) * 0.1)
 
 sample_mean = torch.tensor([0.0, 0.0], device="cuda").reshape(1, 1, d, 1)
 samples = means.unsqueeze(-1) - sample_mean
 powers = -0.5 * (samples.transpose(-1, -2) @ (conic @ samples))
-values = torch.exp(powers)
+values = torch.exp(powers).squeeze(-1)
 values = values / torch.max(values)
 
 scaling = torch.exp(scaling)
 transform = f.tanh(transform)
 
 covariances = gaussians.build_covariances(scaling, transform)
+conics = torch.inverse(covariances)
 
 res = 64
 tx = torch.linspace(-1, 1, res).cuda()
 ty = torch.linspace(-1, 1, res).cuda()
 gx, gy = torch.meshgrid((tx, ty), indexing="xy")
 samples = torch.stack((gx, gy), dim=-1).reshape(res * res, d)
+samples.requires_grad = True
 
-img = gaussians.gaussian_derivative(means, covariances, opacities, values, samples, d)
-img = img.sum(dim=(1,2)).reshape(res, res, -1).detach().cpu().numpy()
+result = gaussians.sample_gaussians(means, conics, opacities, values, samples)
+grad1 = torch.autograd.grad(result.sum(), samples, retain_graph=True, create_graph=True)[0]
+
+img = gaussians.gaussian_derivative(means, conics, opacities, values, samples)
+img1 = img.sum(dim=(1,2)).reshape(res, res, -1).detach().cpu().numpy()
+img2 = grad1.reshape(res, res, -1).detach().cpu().numpy()
+img3 = img1-img2
+
+fig = plt.figure()
+plt.imshow(result.sum(dim=(1,2)).reshape(res, res).detach().cpu().numpy())
+plt.colorbar()
+plt.savefig("input.png")
+
+gaussians.plot_gaussians(means, covariances, opacities, values)
+plt.savefig("gaussian_plot.png")
 
 fig = plt.figure()
 ax = fig.subplots(1, 2)
 ax[0].set_title("x")
-im = ax[0].imshow(img[:,:,0])
+im = ax[0].imshow(img1[:,:,0])
 plt.colorbar(im)
 ax[1].set_title("y")
-im = ax[1].imshow(img[:,:,1])
+im = ax[1].imshow(img1[:,:,1])
 plt.colorbar(im)
 plt.savefig("derivatives.png")
 
-img = gaussians.gaussian_derivative2(means, covariances, opacities, values, samples, d)
-img = img.sum(dim=(1,2)).reshape(res, res, -1).detach().cpu().numpy()
+fig = plt.figure()
+ax = fig.subplots(1, 2)
+ax[0].set_title("x")
+im = ax[0].imshow(img2[:,:,0])
+plt.colorbar(im)
+ax[1].set_title("y")
+im = ax[1].imshow(img2[:,:,1])
+plt.colorbar(im)
+plt.savefig("derivatives_autograd.png")
 
 fig = plt.figure()
 ax = fig.subplots(1, 2)
 ax[0].set_title("x")
-im = ax[0].imshow(img[:,:,0])
+im = ax[0].imshow(img3[:,:,0])
 plt.colorbar(im)
 ax[1].set_title("y")
-im = ax[1].imshow(img[:,:,1])
+im = ax[1].imshow(img3[:,:,1])
+plt.colorbar(im)
+plt.savefig("derivatives_error.png")
+
+grad2_1 = torch.autograd.grad(grad1[:,0].sum(), samples, retain_graph=True)[0]
+grad2_2 = torch.autograd.grad(grad1[:,1].sum(), samples)[0]
+grad2 = torch.cat((grad2_1, grad2_2), dim=-1)
+
+img = gaussians.gaussian_derivative2(means, conics, opacities, values, samples)
+img1 = img.sum(dim=(1,2)).reshape(res, res, -1).detach().cpu().numpy()
+img2 = grad2.reshape(res, res, -1).detach().cpu().numpy()
+img3 = img1-img2
+
+fig = plt.figure()
+ax = fig.subplots(2, 2)
+ax[0,0].set_title("xx")
+im = ax[0,0].imshow(img1[:,:,0])
+plt.colorbar(im)
+ax[0,1].set_title("xy")
+im = ax[0,1].imshow(img1[:,:,1])
+plt.colorbar(im)
+ax[1,0].set_title("yx")
+im = ax[1,0].imshow(img1[:,:,2])
+plt.colorbar(im)
+ax[1,1].set_title("yy")
+im = ax[1,1].imshow(img1[:,:,3])
 plt.colorbar(im)
 plt.savefig("second_derivatives.png")
+
+fig = plt.figure()
+ax = fig.subplots(2, 2)
+ax[0,0].set_title("xx")
+im = ax[0,0].imshow(img2[:,:,0])
+plt.colorbar(im)
+ax[0,1].set_title("xy")
+im = ax[0,1].imshow(img2[:,:,1])
+plt.colorbar(im)
+ax[1,0].set_title("yx")
+im = ax[1,0].imshow(img2[:,:,2])
+plt.colorbar(im)
+ax[1,1].set_title("yy")
+im = ax[1,1].imshow(img2[:,:,3])
+plt.colorbar(im)
+plt.savefig("second_derivatives_autograd.png")
+
+fig = plt.figure()
+ax = fig.subplots(2, 2)
+ax[0,0].set_title("xx")
+im = ax[0,0].imshow(img3[:,:,0])
+plt.colorbar(im)
+ax[0,1].set_title("xy")
+im = ax[0,1].imshow(img3[:,:,1])
+plt.colorbar(im)
+ax[1,0].set_title("yx")
+im = ax[1,0].imshow(img3[:,:,2])
+plt.colorbar(im)
+ax[1,1].set_title("yy")
+im = ax[1,1].imshow(img3[:,:,3])
+plt.colorbar(im)
+plt.savefig("second_derivatives_error.png")